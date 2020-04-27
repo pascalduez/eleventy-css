e94ee6333b96c5500555f6a15aadcf293916f723
Pascal Duez <public@pascalduez.me>
2020-04-27 16:26:23 +0200

    No class for JS templates

--- js-template/main.11ty.js
+++ js-template/main.11ty.js
@@ -2,19 +2,15 @@ const { readFile } = require('fs').promises;
 const postcssrc = require('postcss-load-config');
 const postcss = require('postcss');
 
-module.exports = class {
-  data() {
-    return {
-      permalink: 'styles/main.css',
-      eleventyExcludeFromCollections: true,
-    };
-  }
+exports.data = () => ({
+  permalink: 'styles/main.css',
+  eleventyExcludeFromCollections: true,
+});
 
-  async render() {
-    let { plugins, options } = await postcssrc();
-    let content = await readFile('styles/index.css');
-    let result = await postcss(plugins).process(content, options);
+exports.render = async () => {
+  let { plugins, options } = await postcssrc();
+  let content = await readFile('styles/index.css');
+  let result = await postcss(plugins).process(content, options);
 
-    return result.css;
-  }
+  return result.css;
 };
